c6c4e067ebde970d25e25f525a2274f46485d588
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-09-30 18:59:46 +0530

    okie

--- Part-2/EventsDOM-A1/data.js
+++ Part-2/EventsDOM-A1/data.js
@@ -1,5 +1,24 @@
-function changeColor(){
+function changeColor1(){
     //document.getElementById('abc').innerHTML="Test"
     document.getElementById('abc').style.backgroundColor="green"
     //alert("Test Case123")
 }
+function changeColor2(){
+    let btnRef = document.getElementsByTagName('button')[1]
+    console.log(btnRef)
+    btnRef.style.backgroundColor="green"
+}
+function changeColor3(){
+    /* let ref= document.querySelector('#btn4')
+    console.log(ref) */
+    document.querySelector('#btn4').style.backgroundColor="yellow"
+}
+/* function changeColor4(){
+    //alert("Test Case123")
+    document.querySelector('.btn3').style.backgroundColor="yellow"
+} */
+
+    function changeColor4(){
+        //alert("Test Case123")
+        document.getElementsByClassName('btn3')[0].style.backgroundColor="yellow"
+    }
---
+++ Part-2/EventsDOM-A2/data.js
@@ -0,0 +1,10 @@
+function changeColor1(){
+    //alert('Test')
+    document.getElementById('abc').style.backgroundColor="blue"
+}
+function changeCase(){
+   //alert("TestCase123")
+    let name=document.getElementById('xyz').value;
+    document.getElementById('xyz').value=name.toUpperCase()
+    
+}
